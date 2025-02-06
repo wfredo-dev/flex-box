948a8bdc873a613d135d759121f40aa435b9603b
wfredo-dev <wfredo.dev@gmail.com>
2025-02-06 13:23:11 +0800

    header ,burger

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+  let burger = document.getElementById("burgermobile");
+  let navBarMobile = document.getElementById("navbarmobile");
+  navBarMobile.classList.toggle("hidden");
+}
